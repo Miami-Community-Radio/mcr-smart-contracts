d67050d0df8dbc2e4e089dfe90baa6fcfed51b2b
Genesis Barrios <genesisbarriosdev@gmail.com>
2023-01-07 23:12:40 -0500

    deploy config

--- test/MCRTest.js
+++ test/MCRTest.js
@@ -1,129 +1,120 @@
-var ElectusProtocol = artifacts.require("./MembershipVerificationToken.sol");
+var MCRProtocol = artifacts.require("./MembershipVerificationToken.sol");
 const truffleAssert = require("truffle-assertions");
 
-contract("ElectusProtocol", function(accounts) {
-  let electusProtocol;
+contract("MCRProtocol", function(accounts) {
+  let MCRProtocol;
   beforeEach("setup", async () => {
-    electusProtocol = await ElectusProtocol.new();
+    MCRProtocol = await MCRProtocol.new();
 
     // 0x626c61636b = hair
     // 0x626c61636b = black
     // 0x7768697465 = white
-    await electusProtocol.addAttributeSet("0x68616972", ["0x626c61636b", "0x7768697465"]);
-    await electusProtocol.addAttributeSet("0x736b696e", ["0x626c61636b", "0x7768697465"]);
-    await electusProtocol.assignTo(accounts[1], [0, 0], {from: accounts[0]});
+    await MCRProtocol.addAttributeSet("0x68616972", ["0x626c61636b", "0x7768697465"]);
+    await MCRProtocol.addAttributeSet("0x736b696e", ["0x626c61636b", "0x7768697465"]);
+    await MCRProtocol.assignTo(accounts[1], [0, 0], {from: accounts[0]});
   });
   it("he is a current member", async () => {
-    const data = await electusProtocol.isCurrentMember(accounts[1]);
+    const data = await MCRProtocol.isCurrentMember(accounts[1]);
     assert.equal(data, true);
   });
   it("he is not a current member", async () => {
-    const data = await electusProtocol.isCurrentMember(accounts[2]);
+    const data = await MCRProtocol.isCurrentMember(accounts[2]);
     assert.equal(data, false);
   });
   it("gets all memebers", async () => {
-    const data = await electusProtocol.getAllMembers();
+    const data = await MCRProtocol.getAllMembers();
     assert.equal(data[0], accounts[1]);
   });
   it("gets current memeber count", async () => {
-    await electusProtocol.assignTo(accounts[2], [0, 0], {from: accounts[0]});
-    await electusProtocol.revokeFrom(accounts[1], {from: accounts[0]});
-    const data = await electusProtocol.getCurrentMemberCount();
+    await MCRProtocol.assignTo(accounts[2], [0, 0], {from: accounts[0]});
+    await MCRProtocol.revokeFrom(accounts[1], {from: accounts[0]});
+    const data = await MCRProtocol.getCurrentMemberCount();
     assert.equal(data, 1);
   });
   it("list of attributes Names", async () => {
-    const data = await electusProtocol.getAttributeNames();
+    const data = await MCRProtocol.getAttributeNames();
     // eslint-disable-next-line no-control-regex
     assert.equal(web3.utils.toAscii(data[1]).replace(/\u0000/g, ""), "skin", 32);
   });
   it("list of attributes of a member", async () => {
-    const data = await electusProtocol.getAttributes(accounts[1]);
+    const data = await MCRProtocol.getAttributes(accounts[1]);
     // eslint-disable-next-line no-control-regex
-    assert.deepEqual(
-      data.map(item => web3.utils.toDecimal(item)),
-      [0, 0]
-    );
+    assert.deepEqual(data.map(item => web3.utils.toDecimal(item)), [0, 0]);
   });
   it("gets attribute of a member by name", async () => {
-    const data = await electusProtocol.getAttributeByIndex(accounts[1], 0);
+    const data = await MCRProtocol.getAttributeByIndex(accounts[1], 0);
     // eslint-disable-next-line no-control-regex
     assert.equal(web3.utils.toDecimal(data), 0);
   });
   it("adds a set of attributes", async () => {
-    await electusProtocol.addAttributeSet(web3.utils.fromAscii("height"), [web3.utils.fromAscii("5"), web3.utils.fromAscii("6")]);
-    const data = await electusProtocol.getAttributeNames();
+    await MCRProtocol.addAttributeSet(web3.utils.fromAscii("height"), [web3.utils.fromAscii("5"), web3.utils.fromAscii("6")]);
+    const data = await MCRProtocol.getAttributeNames();
     // eslint-disable-next-line no-control-regex
     assert.equal(web3.utils.toAscii(data[2]).replace(/\u0000/g, ""), "height", 32);
   });
   it("modifies attribute by index", async () => {
-    const result = await electusProtocol.modifyAttributeByIndex(accounts[1], 0, 0);
-    const data = await electusProtocol.getAttributes(accounts[1]);
+    const result = await MCRProtocol.modifyAttributeByIndex(accounts[1], 0, 0);
+    const data = await MCRProtocol.getAttributes(accounts[1]);
     truffleAssert.eventEmitted(result, "ModifiedAttributes");
     // eslint-disable-next-line no-control-regex
-    assert.deepEqual(
-      data.map(item => web3.utils.toDecimal(item)),
-      [0, 0]
-    );
+    assert.deepEqual(data.map(item => web3.utils.toDecimal(item)), [0, 0]);
   });
   it("request memebership", async () => {
-    const requestedMembership = await electusProtocol.requestMembership([0, 0], {
+    const requestedMembership = await MCRProtocol.requestMembership([0, 0], {
       from: accounts[2]
     });
-    const data = await electusProtocol.isCurrentMember(accounts[2]);
-    const isMembershipPending = await electusProtocol.pendingRequests(accounts[2]);
+    const data = await MCRProtocol.isCurrentMember(accounts[2]);
+    const isMembershipPending = await MCRProtocol.pendingRequests(accounts[2]);
     assert.equal(isMembershipPending, true);
     assert.equal(data, false);
     truffleAssert.eventEmitted(requestedMembership, "RequestedMembership");
   });
   it("request and approve memebership", async () => {
-    const requestedMembership = await electusProtocol.requestMembership([0, 0], {
+    const requestedMembership = await MCRProtocol.requestMembership([0, 0], {
       from: accounts[2]
     });
-    const approvedMembership = await electusProtocol.approveRequest(accounts[2], {
+    const approvedMembership = await MCRProtocol.approveRequest(accounts[2], {
       from: accounts[0]
     });
-    const data = await electusProtocol.isCurrentMember(accounts[2]);
+    const data = await MCRProtocol.isCurrentMember(accounts[2]);
     assert.equal(data, true);
-    const attr = await electusProtocol.getAttributes(accounts[2]);
+    const attr = await MCRProtocol.getAttributes(accounts[2]);
     // eslint-disable-next-line no-control-regex
-    assert.deepEqual(
-      attr.map(item => web3.utils.toDecimal(item)),
-      [0, 0]
-    );
+    assert.deepEqual(attr.map(item => web3.utils.toDecimal(item)), [0, 0]);
     truffleAssert.eventEmitted(requestedMembership, "RequestedMembership");
     truffleAssert.eventEmitted(approvedMembership, "ApprovedMembership");
   });
   it("discard memebership request", async () => {
-    const requestedMembership = await electusProtocol.requestMembership([0, 0], {
+    const requestedMembership = await MCRProtocol.requestMembership([0, 0], {
       from: accounts[2]
     });
-    await electusProtocol.discardRequest(accounts[2], {
+    await MCRProtocol.discardRequest(accounts[2], {
       from: accounts[0]
     });
-    const isMembershipPending = await electusProtocol.pendingRequests(accounts[2]);
-    const data = await electusProtocol.isCurrentMember(accounts[2]);
+    const isMembershipPending = await MCRProtocol.pendingRequests(accounts[2]);
+    const data = await MCRProtocol.isCurrentMember(accounts[2]);
     assert.equal(isMembershipPending, false);
     assert.equal(data, false);
     truffleAssert.eventEmitted(requestedMembership, "RequestedMembership");
   });
   it("self revoke memebership", async () => {
-    const revoke = await electusProtocol.forfeitMembership({
+    const revoke = await MCRProtocol.forfeitMembership({
       from: accounts[1]
     });
     truffleAssert.eventEmitted(revoke, "Forfeited");
-    const data = await electusProtocol.isCurrentMember(accounts[1]);
+    const data = await MCRProtocol.isCurrentMember(accounts[1]);
     assert.equal(data, false);
   });
   it("owner revokes memebership", async () => {
-    const revoke = await electusProtocol.revokeFrom(accounts[1], {
+    const revoke = await MCRProtocol.revokeFrom(accounts[1], {
       from: accounts[0]
     });
     truffleAssert.eventEmitted(revoke, "Revoked");
-    const data = await electusProtocol.isCurrentMember(accounts[1]);
+    const data = await MCRProtocol.isCurrentMember(accounts[1]);
     assert.equal(data, false);
   });
   it("gets attribute collection", async () => {
-    const data = await electusProtocol.getAttributeExhaustiveCollection(0);
+    const data = await MCRProtocol.getAttributeExhaustiveCollection(0);
     assert.equal("0" + data[1].replace(/0/g, ""), "0x626c61636b");
     assert.equal("0" + data[0].replace(/0/g, ""), "0x756e646566696e6564");
   });
--- truffle-config.js
+++ truffle-config.js
@@ -1,47 +1,32 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
-    coverage: {
-      host: "localhost",
-      port: 8555, // <-- Use port 8555
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01, // <-- Use this low gas price
-      network_id: "*"
-    }
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
+
+  // Set default mocha options here, use special reporters etc.
   mocha: {
-    useColors: true
+    // timeout: 100000
   },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.4.25",
-      settings: {
-        optimizer: {
-          enabled: true, // Default: false
-          runs: 600 // Default: 200
-        }
-      }
+        version: "0.4.25",
     }
-  },
-  plugins: ["solidity-coverage"]
-};
+  }
+}
